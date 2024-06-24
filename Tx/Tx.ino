1eabb421b5454b03d18cf73d6d8a396a22393e2b
Richard Goutorbe <rg@reseau.org>
2024-06-24 15:39:36 +0200

    doc

--- Tx/Tx.ino
+++ Tx/Tx.ino
@@ -26,14 +26,6 @@
  * type Ctrl-A N    to prefix each line with a timestamp
  * type Ctrl-A Z    to display minicom menu
  * type Ctrl-A X    to quit
- *
- * This program is published under the GNU General Public License. 
- * This program is free software and you can redistribute it and/or modify it under the terms
- * of the GNU General  Public License as published by the Free Software Foundation, version 3.
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
- * See the GNU General Public License for more details : https://www.gnu.org/licenses/ *GPL 
- *
- * Installation, usage : https://github.com/rigou/nRF24L01-FHSS/
 */
 
 #include "Common.h"
